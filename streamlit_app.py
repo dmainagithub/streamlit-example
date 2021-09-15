1b3a5d0895d372955190e59aa7e64be40d9a5602
Daniel Maina Nderitu <maina2007wambugu@gmail.com>
2021-09-15 10:37:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,88 @@
-from typing import Container
 import numpy as np
 import pandas as pd
 import streamlit as st
 # from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
-import plotly.express as px  # an API for creating figures 
+import plotly.express as px
+
 import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 
+# Web App Title
+st.markdown('''
+# **OVC Data Visualization App**
+App built using `PYTHON` & `STREAMLIT` by [Daniel Maina]
+---
+''')
 
-app = dash.Dash(__name__)
 
 
-# ----------------------------------------------------------------------------------------------------
-# Import and clean data (importing csv into pandas)
-df = pd.read_csv(r'https://raw.githubusercontent.com/dmainagithub/my_datasets/main/intro_bees.csv')
-
-df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
-df.reset_index(inplace=True)
-print(df[:5])
+# Upload CSV data
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown("""
+    Please download the **example file here** to a local folder then load it into this platform for data visualization:
+[Example CSV input file](https://raw.githubusercontent.com/dmainagithub/my_datasets/main/viral_load_results.csv)
+""")
+# Pandas Profiling Report
+if uploaded_file is not None:
+    @st.cache
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    # pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Pandas Profiling Report**')
+    # st_profile_report(pr)
+    
 
     
-# ----------------------------------------------------------------------------------------------------
+else:
+    st.info('Awaiting for CSV file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        # Example data
+        @st.cache
+        def load_data():
+            a = pd.DataFrame(
+                np.random.rand(100, 5),
+                columns=['a', 'b', 'c', 'd', 'e']
+            )
+            return a
+        df = load_data()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
+
+        
+app = dash.Dash(__name__)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}    
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})    
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+    
 app.layout = html.Div([
     
 html.H1("Web Application Dashboards with Dash", style={'text-align':'center'}),
@@ -44,78 +102,14 @@ html.H1("Web Application Dashboards with Dash", style={'text-align':'center'}),
     html.Div(id='output_container', children=[]),    
     html.Br(),
 
-    dcc.Graph(id='my_bee_map', figure={})
+    dcc.Graph(
+        id='my_bee_map', 
+        figure=fig
+    )
 
 ])
 
-
-    #  ----------------------------------------------------------------------------------------------------------------------------
-    #  Connect the plotly graphs with Dash Components
-    #  ----------------------------------------------------------------------------------------------------------------------------
-@app.callback(
-    [Output(component_id='output_container', component_property='children')],
-    [Output(component_id='my_bee_map', component_property='figure')],
-    [Input(component_id='slct_year', component_property='value')]
-)
-
-def update_graph(option_slctd):
-    print(option_slctd)
-    print(type(option_slctd))
-
-    container = "The year chosen by user was:  {}".format(option_slctd)
-
-    dff = df.copy()
-    dff = dff[dff["Year"] == option_slctd]
-    dff = dff[dff["Affected by"] == "Varroa_mites"]
-
-    # Plotly Express
-    fig = px.choropleth(
-        data_frame=dff,
-        locationmode='USA-states',
-        locations='state_code',
-        scope="usa",
-        color='Pct of Colonies Impacted',
-        hover_data=['State','Pct of Colonies Impacted'],
-        color_continuous_scale=px.colors.sequential.Viridis,
-        labels={'Pct of Colonies Impacted': '% of Bee colonies'},
-        template='plotly_dark'
-    )
-
-    # # Plotly Graph Objects (GO)
-    # fig = go.Figure(
-    #     data=[go.Choropleth(
-    #         locationmode='USA-states',
-    #         locations=dff['state_code'],
-    #         z=dff['Pct of COlonies Impacted'].astype(float),
-    #         colorscale='Reds',
-    #     )]
-    # )
-
-    # fig.update_layout(
-    #     title_text="Bees Affected by Mites in the USA",
-    #     title_xanchor="center",
-    #     title_font=dict(size=24),
-    #     title_x=0.5,
-    #     geo=dict(scope='usa'),
-    # )
-
-    return container, fig
-
-
-
-
-    #  ----------------------------------------------------------------------------------------------------------------------------
-    #  ----------------------------------------------------------------------------------------------------------------------------
-   
 if __name__ == '__main__':
      app.run_server(debug=True)
-
-    #  ----------------------------------------------------------------------------------------------------------------------------
-    #  ----------------------------------------------------------------------------------------------------------------------------
-    #  ----------------------------------------------------------------------------------------------------------------------------
-    #  ----------------------------------------------------------------------------------------------------------------------------
-    #  ----------------------------------------------------------------------------------------------------------------------------
-    #  ----------------------------------------------------------------------------------------------------------------------------
     
-
-
+  
